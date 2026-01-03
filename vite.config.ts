ddb9735591da558a75030d8015831345701bb1bc
inoha_kudo <inoha1062@gmail.com>
2026-01-03 17:20:33 +0900

    [fix] Webフォントを外部CDN参照からバンドルに変更

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import tailwindcss from '@tailwindcss/vite';
 import vue from '@vitejs/plugin-vue';
 import laravel from 'laravel-vite-plugin';
 import { defineConfig } from 'vite';
+import webfontDownload from 'vite-plugin-webfont-dl';
 
 export default defineConfig({
     plugins: [
@@ -23,5 +24,6 @@ export default defineConfig({
         wayfinder({
             formVariants: true,
         }),
+        webfontDownload('https://fonts.bunny.net/css?family=instrument-sans:400,500,600'),
     ],
 });
