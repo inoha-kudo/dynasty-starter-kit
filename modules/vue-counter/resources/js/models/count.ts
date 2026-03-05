445412d40c7da02b434e6e0518f0777ff5ab6d1a
inoha_kudo <inoha1062@gmail.com>
2026-03-06 00:23:41 +0900

    ウィp

--- modules/vue-counter/resources/js/models/count.ts
+++ modules/vue-counter/resources/js/models/count.ts
@@ -1,5 +1,11 @@
 export class Count {
-    private constructor(public readonly value: number) {}
+    public static readonly EXCEPTION_MESSAGE_VALUE_MUST_BE_INTEGER = 'The provided value (%s) must be an integer.';
+
+    private constructor(public readonly value: number) {
+        if (!Number.isInteger(value)) {
+            throw new TypeError(Count.EXCEPTION_MESSAGE_VALUE_MUST_BE_INTEGER.replace('%s', value.toString()));
+        }
+    }
 
     static of(value: number): Count {
         return new Count(value);
--- modules/vue-counter/resources/test/models/count.test.ts
+++ modules/vue-counter/resources/test/models/count.test.ts
@@ -18,4 +18,21 @@ describe('Count', () => {
         const resetCount = Count.of(initial);
         expect(resetCount.value).toBe(10);
     });
+
+    it('can be negative', () => {
+        const count = Count.of(-1);
+        expect(count.value).toBe(-1);
+    });
+
+    it('can decrement below zero', () => {
+        const count = Count.of(0);
+        expect(count.decrement().value).toBe(-1);
+    });
+
+    it('must be an integer', () => {
+        const invalidValue = 1.5;
+        expect(() => Count.of(invalidValue)).toThrow(TypeError);
+        expect(() => Count.of(invalidValue)).toThrow(Count.EXCEPTION_MESSAGE_VALUE_MUST_BE_INTEGER.replace('%s', invalidValue.toString()));
+        expect(() => Count.of(0.1)).toThrow(TypeError);
+    });
 });
