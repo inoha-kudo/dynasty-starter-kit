939085c37fb965bf3973950588cd6b3884fa7b26
inoha_kudo <inoha1062@gmail.com>
2026-03-08 17:20:37 +0900

    wip

--- modules/vue-counter/resources/js/tests/composables/useCounter.test.ts
+++ modules/vue-counter/resources/js/tests/composables/useCounter.test.ts
@@ -1,37 +1,45 @@
-import { expect, test } from 'vitest';
 import { useCounter } from '../../composables/useCounter';
 
-test('initial value can be set', () => {
-    const { count } = useCounter(5);
-    expect(count.value).toBe(5);
-});
+describe('useCounter', () => {
+    test('count', () => {
+        const value = 0;
 
-test('default initial value is 0', () => {
-    const { count } = useCounter();
-    expect(count.value).toBe(0);
-});
+        const { count } = useCounter(value);
 
-test('can increment', () => {
-    const { count, increment } = useCounter(0);
-    increment();
-    expect(count.value).toBe(1);
-});
+        expect(count.value).toBe(value);
+    });
 
-test('can decrement', () => {
-    const { count, decrement } = useCounter(10);
-    decrement();
-    expect(count.value).toBe(9);
-});
+    test('increment', () => {
+        const value = 0;
 
-test('can reset', () => {
-    const { count, increment, reset } = useCounter(5);
-    increment();
-    expect(count.value).toBe(6);
-    reset();
-    expect(count.value).toBe(5);
-});
+        const { count, increment } = useCounter(value);
 
-test('counter ref is NOT exposed', () => {
-    const result = useCounter(0);
-    expect(result).not.toHaveProperty('counter');
+        increment();
+
+        expect(count.value).toBe(value + 1);
+    });
+
+    test('decrement', () => {
+        const value = 0;
+
+        const { count, decrement } = useCounter(value);
+
+        decrement();
+
+        expect(count.value).toBe(value - 1);
+    });
+
+    test('reset', () => {
+        const value = 1;
+
+        const { count, increment, reset } = useCounter(value);
+
+        increment();
+
+        expect(count.value).toBe(value + 1);
+
+        reset();
+
+        expect(count.value).toBe(value);
+    });
 });
