7576577ec7860642ddb73648c1dc7b86be899351
inoha_kudo <inoha1062@gmail.com>
2026-03-22 23:09:12 +0900

    wip

---
+++ app-modules/dynasty-hello/resources/js/pages/Index.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import HelloContent from '@dynasty/hello/components/HelloContent.vue';
+import { Head } from '@inertiajs/vue3';
+
+defineProps<{
+    message: string;
+}>();
+</script>
+
+<template>
+    <Head title="Hello" />
+    <HelloContent :message="message" />
+</template>
